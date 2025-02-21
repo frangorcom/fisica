f53ab974a1b161a3b45ed3702dc4929f7041ea53
Paco <pacoman1608@gmail.com>
2025-02-20 20:31:29 -0600

    Primer commit

--- java/posicion.js
+++ java/posicion.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   var typed = new Typed('#pos', {
-    strings: ['¿qué es un vector euclidiano?','¿para qué nos sirve?','¿cómo se representa?'], // Textos que se mostrarán
+    strings: ['¿para qué nos sirve?','¿cómo se representa?'], // Textos que se mostrarán
     typeSpeed: 100, // Velocidad de escritura
     backSpeed: 15, // Velocidad de borrado
     loop: true, // Repetir el efecto
---
+++ java/uni.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var typed = new Typed('#typed', {
+    strings: ['Vectores', 'Vector Unitario'], // Textos que se mostrarán
+    typeSpeed: 50, // Velocidad de escritura
+    backSpeed: 25, // Velocidad de borrado
+    loop: true, // Repetir el efecto
+  });
+});
